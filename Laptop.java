bf1c1b004578745bd8b3361e7bcaf4683ba5b5ac
Buzynnikov <buzynnikov2111@gmail.com>
2023-06-24 20:20:35 +0500

    complete

---
+++ Laptop.java
@@ -0,0 +1,19 @@
+package HW_6;
+
+public class Laptop{
+    int id;
+    String name;
+    double price;
+    String manufacturer;
+    double screenDiagonal;
+    int ram;
+    boolean gamingLaptop;
+    String processor;
+    String videoCard;
+    int memory;
+    String OS;
+    int cores;
+    String screenType;
+    String processorManufacturer;
+    
+}
